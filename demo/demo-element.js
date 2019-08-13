98736f00dab63b6a2af1d6593440aef9219f9760
Nimral Baldaniya <nirmalbaldaniya.dream@gmail.com>
2019-08-13 05:19:52 +0000

    misc improvement changes.

---
+++ demo/demo-element.js
@@ -0,0 +1,43 @@
+import { LitElement, html, css } from 'lit-element'
+import '../dw-text-editor';
+
+class DemoElement extends LitElement {
+  static get styles() {
+    return [
+      css`
+        :host{
+          display: block;
+        }
+
+        .editor-container{
+          width: 100%;
+          height: 250px;
+          border: 1px solid black;
+          position: relative;
+        }
+      `
+    ]
+  }
+  render() {
+    return html`
+      <h2>dw-text-editor Demo:</h2>
+      <h3>Edit Mode</h3>
+      <div class="editor-container">
+        <dw-text-editor value="Hello world.." @value-changed="${this._valueChange}"></dw-text-editor>
+      </div>
+
+      <h3>Readonly Mode</h3>
+      <div class="editor-container">
+        <dw-text-editor readonly value="Hello world.."></dw-text-editor>
+      </div>
+    `
+  }
+
+  _valueChange(e) {
+    console.log(e.detail.value);
+  }
+
+
+}
+
+customElements.define('demo-element', DemoElement);
--- readme.md
+++ readme.md
@@ -5,17 +5,17 @@
 
 ## Installation Steps
 1. `npm i @dw/dw-text-editor`
-2. Copy the contents of the `Squire/` directory onto your server.
+2. Copy the `squire.html` file into your server root directory.
 
 ## Usage
 ### Import
 - `import '@dw/dw-text-editor'`
 
 ### Use
-- Set `iFramePath` attribute  to `path/to/squire.html` (which copied in Installation Steps)
+- Set `iframePath` attribute  to `path/to/squire.html` if `squire.html` file isn't on root directory.
 - ```
   <dw-text-editor 
-    iFramePath="path/to/squire.html" 
+    iframePath="path/to/squire.html" 
     value="" readonly>
   </dw-text-editor>
   ```
